770588524b9f3d7c478ae78f19c5a40fe8edda1e
朱楠 <donar_zhu@163.com>
2017-09-21 14:17:30 +0800

    webbrowser connected success

--- CellCenter.aicyber/MainWindow.xaml.cs
+++ CellCenter.aicyber/MainWindow.xaml.cs
@@ -20,9 +20,40 @@ namespace CellCenter.aicyber
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private bool isFirstload = true;
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Normal;
+            this.WindowStyle = System.Windows.WindowStyle.ThreeDBorderWindow;
+            this.ResizeMode = System.Windows.ResizeMode.CanResize;
+            this.Topmost = true;
+
+            this.Left = 0.0;
+            this.Top = 0.0;
+            this.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
+            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+
+            if (isFirstload)
+            {
+                
+               this.mBrowser.Navigate("http://121.42.36.138:8000/static/dist/index.html#/Login");
+                isFirstload = false;
+            }
+
+            SizeChanged += MainWindow_SizeChanged;
+
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            int iBreak = 0;
         }
     }
 }
